ee0c64a70c95571b7b9b03f5974234c5bcbdf50c
Igor Urvantsev <igor.urvantsev@gmail.com>
2022-12-16 19:36:48 +0300

    функция поиска НОД get_gcd_recur() с рекурсией

--- main.py
+++ main.py
@@ -21,11 +21,27 @@ def get_gcd(num1: int, num2: int) -> int:
                 a, b = b, rest
         return b
 
+def get_gcd_recur(num1: int, num2: int) -> int:
+    '''
+    :param num1: первое число
+    :param num2: второе число
+    :return: НОД по алгоритму Евклида рекурсией
+    '''
+
+    try:
+        rest = max(num1, num2) % min(num1, num2)
+    except ZeroDivisionError:
+        raise
+    else:
+        if rest == 0:
+            return min(num1, num2)
+        else:
+            return get_gcd_recur(min(num1, num2), rest)
 
 # ----- ASSERT ------
 # get_gcd()
 gcd_nums = ((140, 96, 4), (16, 24, 8), (15, 60, 15), (10, 15, 5), (45, 56, 1), (21, 49, 7), (2, 7, 1))
 assert all(get_gcd(x, y) == z for (x, y, z) in gcd_nums), '!WARNING! Некорректная отработка get_gcd()'
-
+assert all(get_gcd_recur(x, y) == z for (x, y, z) in gcd_nums), '!WARNING! Некорректная отработка get_gcd_recur()'
 
 
