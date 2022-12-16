268264073a630727191c562b2847d2411876e421
Igor Urvantsev <igor.urvantsev@gmail.com>
2022-12-16 21:27:59 +0300

    базовое описание класса Rational (__init()__, shorten())

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+class NotNatural(Exception):
+    pass
+
 def get_gcd(num1: int, num2: int) -> int:
     '''
     Функция поиска НОД по Эвклиду
@@ -38,10 +41,69 @@ def get_gcd_recur(num1: int, num2: int) -> int:
         else:
             return get_gcd_recur(min(num1, num2), rest)
 
+
+class Rational:
+    '''
+    Класс рациональных чисел
+    '''
+
+    def __init__(self, num: int, den: int):
+        '''
+        :param num: числитель - целое число
+        :param den: знаменатель - натуральное число
+        '''
+
+        try:
+            # знаменатель - натуральное число
+            if int(den) <= 0:
+                # добавить обработчик исключения
+                raise NotNatural
+
+            num = int(num)
+            # установка знака дроби
+            if num > 0:
+                self.sign = 1
+            elif num < 0:
+                self.sign = -1
+            else:
+                self.sign = 0
+
+            self.num, self.den = abs(num), abs(den)
+
+            # сокращение дроби
+            self.shorten()
+        except NotNatural:
+            # добавить обработчик исключения
+            raise
+        except TypeError:
+            # добавить обработчик исключения
+            raise
+        except ValueError:
+            # добавить обработчик исключения
+            raise
+
+    def shorten(self):
+        try:
+            gcd = get_gcd(self.num, self.den)
+            if gcd > 1:
+                self.num //= gcd
+                self.den //= gcd
+        except:
+            # добавить обработчик исключения
+            pass
+
+
 # ----- ASSERT ------
 # get_gcd()
 gcd_nums = ((140, 96, 4), (16, 24, 8), (15, 60, 15), (10, 15, 5), (45, 56, 1), (21, 49, 7), (2, 7, 1))
 assert all(get_gcd(x, y) == z for (x, y, z) in gcd_nums), '!WARNING! Некорректная отработка get_gcd()'
 assert all(get_gcd_recur(x, y) == z for (x, y, z) in gcd_nums), '!WARNING! Некорректная отработка get_gcd_recur()'
-
-
+# class Rational
+test_fraction_1 = Rational(-3, 4)
+assert test_fraction_1.num == 3 and test_fraction_1.den == 4 and test_fraction_1.sign == -1, '!WARNING! Некорректная отработка Rational'
+test_fraction_1 = Rational(-0, 4)
+assert test_fraction_1.num == 0 and test_fraction_1.den == 4 and test_fraction_1.sign == 0, '!WARNING! Некорректная отработка Rational'
+test_fraction_1 = Rational(3, 4)
+assert test_fraction_1.num == 3 and test_fraction_1.den == 4 and test_fraction_1.sign == 1, '!WARNING! Некорректная отработка Rational'
+test_fraction_2 = Rational(18, 36)
+assert test_fraction_2.num == 1 and test_fraction_2.den == 2 and test_fraction_2.sign == 1, '!WARNING! Некорректная отработка сокращения дроби в shorten() в Rational'
